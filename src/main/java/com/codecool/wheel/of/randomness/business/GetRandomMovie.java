0fadbc72c1cf69f0006914b2817b070a2e70be07
András Kelle <kelleandras@gmail.com>
2021-01-18 13:20:49 +0100

    feature: add endpoints

--- src/main/java/com/codecool/wheel/of/randomness/business/GetRandomMovie.java
+++ src/main/java/com/codecool/wheel/of/randomness/business/GetRandomMovie.java
@@ -1,4 +1,37 @@
 package com.codecool.wheel.of.randomness.business;
 
-public class GetRandomMovie {
+import com.codecool.wheel.of.randomness.model.Movie;
+import org.json.simple.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/movies/"})
+public class GetRandomMovie extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Movie movie = new Movie();
+
+
+        PrintWriter out = resp.getWriter();
+        /*
+        JSONObject responseJson = new JSONObject();
+        responseJson.put("title", movie.getTitle());
+        responseJson.put("year", movie.getYear());
+        responseJson.put("overview", movie.getOverview());
+        responseJson.put("runtime", movie.getRuntime());
+        responseJson.put("trailer", movie.getTrailer());
+        responseJson.put("homepage", movie.getHomepage());
+        responseJson.put("rating", movie.getRating());
+        //TODO "google" an image by title and returns the url as json
+
+        String response = responseJson.toJSONString();
+         */
+        out.println("it's a random movie as JSON Object!");
+    }
 }
--- src/main/java/com/codecool/wheel/of/randomness/controller/MainController.java
+++ src/main/java/com/codecool/wheel/of/randomness/controller/MainController.java
@@ -1,4 +1,22 @@
 package com.codecool.wheel.of.randomness.controller;
 
-public class MainController {
+
+import com.codecool.wheel.of.randomness.model.Movie;
+import org.json.simple.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/"})
+public class MainController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("it's an awesome page!");
+    }
 }
