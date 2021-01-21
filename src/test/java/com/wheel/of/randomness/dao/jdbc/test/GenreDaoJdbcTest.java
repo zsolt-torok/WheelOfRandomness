69d6f799328d07fc72502b6781dcc9431360ea6e
András Kelle <kelleandras@gmail.com>
2021-01-21 11:41:51 +0100

    feature: add test for GenreDaoJdbc

--- src/test/java/com/wheel/of/randomness/dao/jdbc/test/GenreDaoJdbcTest.java
+++ src/test/java/com/wheel/of/randomness/dao/jdbc/test/GenreDaoJdbcTest.java
@@ -1,13 +1,24 @@
 package com.wheel.of.randomness.dao.jdbc.test;
 
+import com.codecool.wheel.of.randomness.config.DataManager;
+import com.codecool.wheel.of.randomness.dao.jdbc.GenreDaoJdbc;
+import com.codecool.wheel.of.randomness.model.Genre;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GenreDaoJdbcTest {
+    GenreDaoJdbc testJDBC = new GenreDaoJdbc(DataManager.connectDataBase());
+
+
+    @Test
+    void getAll_ReturnsAllRowsFromDb() {
+        int genreTableSize = 21;
+        List<Genre> allGenre = testJDBC.getAll();
+        assertEquals(genreTableSize, allGenre.size());
+    }
+
+
 }
